db9d6be0eecd16e827218901232321e6cfbc0b46
M Asad Rizvi <asadraoof023@gmail.com>
2024-09-02 18:12:43 +0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -17,8 +17,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(cors());
-
 app.use("/api/v1/", uploadRouter);
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/project", projectRouter);
