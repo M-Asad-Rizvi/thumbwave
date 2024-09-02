20136e79400562fd0727af6e1d78b1ab6da9d19c
M Asad Rizvi <asadraoof023@gmail.com>
2024-09-02 10:11:02 +0500

    [feature]: New Commit

--- server/app.js
+++ server/app.js
@@ -17,12 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(
-   cors({
-      credentials: true,
-      origin: process.env.CLIENT_URL,
-   })
-);
+app.use(cors());
 
 app.use("/api/v1/", uploadRouter);
 app.use("/api/v1/user", userRouter);
