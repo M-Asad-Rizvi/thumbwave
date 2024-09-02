c2cf3e4c9ca523b905865baf18c17e7107476ea2
M Asad Rizvi <asadraoof023@gmail.com>
2024-09-02 11:37:51 +0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -17,7 +17,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(cors());
+app.use(
+   cors({
+      origin: "https://the-thumbwave.vercel.app",
+      credentials: true,
+   })
+);
 
 app.use("/api/v1/", uploadRouter);
 app.use("/api/v1/user", userRouter);
