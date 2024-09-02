5c9245d6e0c6471976d4f345aea59d3b844eeffa
M Asad Rizvi <asadraoof023@gmail.com>
2024-09-02 09:36:52 +0500

    [feature]: Most Final

--- server/app.js
+++ server/app.js
@@ -17,7 +17,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(cors());
+app.use(
+   cors({
+      credentials: true,
+      origin: process.env.CLIENT_URL,
+   })
+);
 
 app.use("/api/v1/", uploadRouter);
 app.use("/api/v1/user", userRouter);
